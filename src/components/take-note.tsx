f61bec0ccc528f90fcd6e3bfb7a64511ee0df3e1
Antony Jaison <antonyjaison639@gmail.com>
2024-03-01 20:21:08 +0530

    added markdown compiler

--- src/components/take-note.tsx
+++ src/components/take-note.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Calendar } from 'lucide-react'
+import Link from 'next/link'
 
 const TakeNote = () => {
   return (
-    <div className='w-[290px] h-fit flex items-center gap-7 bg-white px-6 py-3 rounded-2xl shadow-link cursor-pointer'>
+    <Link href="/add-note" className='w-[290px] h-fit flex items-center gap-7 bg-white px-6 py-3 rounded-2xl shadow-link cursor-pointer'>
         <div className=' bg-primary w-fit rounded-full p-3'>
             <Calendar size={20} color='#1A1A1A'/>
         </div>
@@ -11,7 +12,7 @@ const TakeNote = () => {
             <p className='text-[#7B7B7B] text-xs font-light'>New note</p>
             <h4 className='text-[#1A1A1A] text-lg font-light'>Take a note</h4>
         </div>
-    </div>
+    </Link>
   )
 }
 
