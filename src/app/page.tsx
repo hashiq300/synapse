bc3b89229debf814dd6a5db6170b0f81eea338aa
Antony Jaison <antonyjaison639@gmail.com>
2024-03-01 15:47:15 +0530

    added navbar and other pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import Sidebar from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <main>
-      hello
+    <main className=" bg-[#FFF7E9]">
+      <Sidebar/>
     </main>
   );
 }
